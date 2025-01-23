719b82485ccf533d21eade7a001925980489f276
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-01-22 22:55:49 -0500

    todolist and form components

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import axios from 'axios'
+import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
@@ -65,23 +67,19 @@ export default class App extends React.Component {
     return (
       <div>
         <div id="error" >Error: {this.state.error}</div>
-        <div id="todos" >
-          <h2>Todos:</h2>
-          {
-            this.state.todos.reduce( (acc, td) => {
-              if(this.state.displayCompleteds || !td.completed) return acc.concat(
-                <div onClick={this.toggleCompleted(td.id)} key={td.id}   >{td.name}{td.completed ? ' ✔️' : ''}</div>
-              )
-              return acc
-            },[])
-          }
-          {/* return <div onClick={this.toggleCompleted(td.id)} key={td.id}   >{td.name}{td.completed ? ' ✔️' : ''}</div> */}
-        </div>
-        <form id="todoForm" onSubmit={this.onTodoFormSubmit} >
-          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type="text" placeholder="Type todo"  ></input>
-          <input type="submit" ></input>
-        </form>
-          <button onClick={this.toggleDisplayCompleteds} >{this.state.displayCompleteds ? 'Hide' : "Show"} Completed</button>
+        <TodoList 
+          todos={this.state.todos}
+          displayCompleteds={this.state.displayCompleteds}
+          toggleCompleted={this.toggleCompleted}
+        />
+
+        <Form 
+          onTodoFormSubmit={this.onTodoFormSubmit}
+          onTodoNameInputChange={this.onTodoNameInputChange}
+          toggleDisplayCompleteds={this.toggleDisplayCompleteds}
+          todoNameInput={this.state.todoNameInput}
+          displayCompleteds={this.state.displayCompleteds}
+        />
       </div>
     )
   }
